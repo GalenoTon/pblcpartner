cc6eaa97aef5bcb779395fe070a19832e1d4c141
GaleTon <well.galeno@gmail.com>
2025-08-01 18:43:41 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,12 @@
 import type { NextConfig } from "next";
 
-<<<<<<< HEAD
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
-module.exports = nextConfig;
-//
-=======
->>>>>>> 5a0297f32605967c3986949a85564d5d7804bab3
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
-
-
-module.exports = {
   images: {
     domains: ['images.unsplash.com'],
   },
-}
-
-
+};
 
 export default nextConfig;
